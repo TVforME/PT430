48421a31de31c048a2cbacbcf602f3b8d40ef04a
VK3DG <168706311+TVforME@users.noreply.github.com>
2025-05-16 23:22:04 +1000

    Create version.h
    Initial upload of working code

---
+++ src/version.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2024  Robert Hensel VK3DG <vk3dgtv@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ version.h  used at build time to add build date and time.
+ 
+ */
+
+
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJOR    1
+#define VERSION_MINOR    0
+#define VERSION_PATCH    0
+#define VERSION_STRING   "1.0.0"
+#define BUILD_DATE      "Fri 16/05/2025"
+#define BUILD_TIME      "22:21:03.35"
+#define BUILD_YEAR      "2025"
+
+#endif // VERSION_H
